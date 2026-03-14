c483b4f255c1e731fa555330e8ca571d0dbb10ff
Dane <gurdane2000@gmail.com>
2026-03-13 22:24:11 -0500

    restrict buzz-in to face buttons

--- index.js
+++ index.js
@@ -31,55 +31,34 @@ const visualize = (joyCon, packet) => {
   } = packet;
 
 
-  console.info(joyCon.values(), joyCon);
+  console.log(joyCon.values(), joyCon);
 
-  if (buttons.a || buttons.b || buttons.x || buttons.y || buttons.up || buttons.down || buttons.left || buttons.right || buttons.l || buttons.r || buttons.zl || buttons.zr) {
-    buzzInSound.play();
-  }
 
   // test led and rumble
   if (buttons.a || buttons.up) {
     joyCon.blinkLED(0);
+    buzzInSound.play();
   }
   if (buttons.b || buttons.down) {
     joyCon.setLED(0);
+    buzzInSound.play();
   }
   if (buttons.x || buttons.right) {
     joyCon.resetLED(0);
     joyCon.rumble(600, 600, 0);
+    buzzInSound.play();
   }
   if (buttons.y || buttons.left) {
     joyCon.rumble(600, 600, 0.5);
+    buzzInSound.play();
   }
-  if (buttons.home) {
+  if (buttons.home || buttons.capture) {
     //joyCon.setHomeLED(true);
     joyCon.setHomeLEDPattern(5, 1, 15, []);
+    buzzInSound.play();
   }
 
-  if (showDebug.checked) {
-    const controller = joyCon instanceof JoyConLeft ? debugLeft : debugRight;
-    controller.querySelector('pre').textContent =
-      `${JSON.stringify(orientation, null, 2)}
-    ${JSON.stringify(orientationQuaternion, null, 2)}
-    ${JSON.stringify(gyroscope, null, 2)}
-    ${JSON.stringify(accelerometer, null, 2)}
-    `;
-    const meterMultiplier = 300;
-    controller.querySelector('#acc-x').value =
-      accelerometer.x * meterMultiplier;
-    controller.querySelector('#acc-y').value =
-      accelerometer.y * meterMultiplier;
-    controller.querySelector('#acc-z').value =
-      accelerometer.z * meterMultiplier;
 
-    const gyroscopeMultiplier = 300;
-    controller.querySelector('#gyr-x').value =
-      gyroscope.rps.x * gyroscopeMultiplier;
-    controller.querySelector('#gyr-y').value =
-      gyroscope.rps.y * gyroscopeMultiplier;
-    controller.querySelector('#gyr-z').value =
-      gyroscope.rps.z * gyroscopeMultiplier;
-  }
 };
 
 // Joy-Cons may sleep until touched, so attach the listener dynamically.
