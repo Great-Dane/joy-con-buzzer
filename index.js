19e4146713e528064d849f2494bded4142853d64
Dane <gurdane2000@gmail.com>
2026-03-14 17:33:53 -0500

    display which controller has buzzed in

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ const connectButton = document.querySelector('#connect-joy-cons');
 const connectButtonRingCon = document.querySelector('#connect-ring-con');
 const debugLeft = document.querySelector('#debug-left');
 const debugRight = document.querySelector('#debug-right');
+const debugGeneral = document.querySelector('.debug-general');
 const showDebug = document.querySelector('#show-debug');
 const rootStyle = document.documentElement.style;
+
+const controllerMap = {
+  8198: 'Mikaela',
+  8199: 'Dane',
+};
 
 connectButton.addEventListener('click', connectJoyCon);
 
 const buzzInSound = new Audio('assets/buzz-in.mp3');
+let isPlayerBuzzed = false;
 
 const visualize = (joyCon, packet) => {
   if (!packet?.actualOrientation) {
@@ -29,6 +36,8 @@ const visualize = (joyCon, packet) => {
     actualOrientationQuaternion: orientationQuaternion,
     ringCon,
   } = packet;
+  const productId = joyCon.device.productId;
+
 
 
   // test led and rumble
@@ -62,6 +71,8 @@ const visualize = (joyCon, packet) => {
       ${JSON.stringify(joyCon.device.productId, null, 2)}
       ${JSON.stringify(buttons, null, 2)}
     `;
+    debugGeneral.querySelector('pre').textContent = `${controllerMap[productId] || 'Unknown Controller'} has buzzed in!`;
+    
   }
 
 };
